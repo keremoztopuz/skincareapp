6ace59c765e13f8f67d9a1e1a369809fbf81e94a
keremozt <keremoztopuzzz@gmail.com>
2026-03-24 11:17:58 +0300

    ML model implementation done

--- SkinCare/Resources/MLManager.swift
+++ SkinCare/Resources/MLManager.swift
@@ -22,4 +22,20 @@ class MLManager {
             print("model not loaded")
         }
     }
+    
+    func detect(image: UIImage, completion: @escaping (SkinCondition?, Double) -> Void ) {
+        guard let model = model, let ciImage = CIImage(image: image) else { return }
+        
+        let request = VNCoreMLRequest(model: model) { request, _ in
+            guard let results = request.results as? [VNClassificationObservation],
+                  let topResult = results.first else {
+                completion(nil, 0)
+                return
+            }
+            let condition = SkinCondition(rawValue: topResult.identifier)
+            completion (condition, Double(topResult.confidence))
+        }
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        try? handler.perform( [request] )
+    }
 }
