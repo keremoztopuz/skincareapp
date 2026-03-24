1a3fb63fec6f5320c747941abfdc9b7246cb049f
keremozt <keremoztopuzzz@gmail.com>
2026-03-24 10:55:51 +0300

    feat: ML model file introduced to project

--- SkinCare/Resources/MLManager.swift
+++ SkinCare/Resources/MLManager.swift
@@ -5,4 +5,21 @@
 //  Created by Kerem Öztopuz on 23.03.2026.
 //
 
-import Foundation
+import CoreML
+import Vision
+import UIKit
+
+class MLManager {
+    
+    private var model : VNCoreMLModel!
+    
+    init() {
+        do {
+            let config = MLModelConfiguration()
+            let coreModel = try skin_disease(configuration: config)
+            self.model = try VNCoreMLModel(for: coreModel.model)
+        } catch {
+            print("model not loaded")
+        }
+    }
+}
