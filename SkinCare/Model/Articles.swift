4aabe077946acb521b4458accd2d819d6063f9ff
keremozt <keremoztopuzzz@gmail.com>
2026-03-24 10:54:04 +0300

    feat: article structure built

--- SkinCare/Model/Articles.swift
+++ SkinCare/Model/Articles.swift
@@ -5,4 +5,13 @@
 //  Created by Kerem Öztopuz on 23.03.2026.
 //
 
+// MARK: Article structure
 import Foundation
+
+struct Articles: Identifiable {
+    let id : UUID
+    let title : String
+    let content : String
+    let category : String
+    let imageName : String
+}
