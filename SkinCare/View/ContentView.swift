efa3ff3ddc3422a0dfd9e61d3ebdc19b9b072ca2
keremozt <keremoztopuzzz@gmail.com>
2026-04-18 21:03:42 +0300

    refactor: use AppStorage directly in ContentView for onboarding and profile state management

--- SkinCare/View/ContentView.swift
+++ SkinCare/View/ContentView.swift
@@ -5,15 +5,16 @@ import SwiftUI
 // FOR NOW, JUST RESETS THE PAGE ON EVERY BUILD.
 
 struct ContentView: View {
-    @AppStorage("hasCompletedOnBoarding") private var
-      hasCompletedOnBoarding = false
-    @AppStorage("hasCompletedProfile") private var
-      hasCompletedProfile = false
+    @AppStorage("hasCompletedOnBoarding") private var hasCompletedOnBoarding = false
+    @AppStorage("hasCompletedProfile") private var hasCompletedProfile = false
+
     var body: some View {
         if !hasCompletedOnBoarding {
             OnBoardingView()
         } else if !hasCompletedProfile {
             ProfileSetupView()
+        } else {
+            Text("Main App")
         }
     }
 }
