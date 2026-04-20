e1dd644819286915230c400e0edcc4718a6eaa25
keremozt <keremoztopuzzz@gmail.com>
2026-04-20 17:49:40 +0300

    feat: add ContentViewModel with AppState enum and navigation logic

--- SkinCare/ViewModel/ContentViewModel.swift
+++ SkinCare/ViewModel/ContentViewModel.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import SwiftUI
+internal import Combine
+
+enum AppState {
+    case onboarding
+    case profileSetup
+    case mainApp
+}
+
+class ContentViewModel: ObservableObject {
+      @Published var hasCompletedOnBoarding: Bool {
+          didSet {
+                                                      
+  UserDefaults.standard.set(hasCompletedOnBoarding,
+  forKey: "hasCompletedOnBoarding")
+          }
+      }
+      @Published var hasCompletedProfile: Bool {
+          didSet {
+   
+  UserDefaults.standard.set(hasCompletedProfile,
+  forKey: "hasCompletedProfile")
+          }
+      }
+
+      var currentState: AppState {
+          if !hasCompletedOnBoarding {
+              return .onboarding
+          } else if !hasCompletedProfile {
+              return .profileSetup
+          } else {
+              return .mainApp
+          }
+      }
+
+      init() {
+          self.hasCompletedOnBoarding =
+  UserDefaults.standard.bool(forKey:
+  "hasCompletedOnBoarding")
+          self.hasCompletedProfile =
+  UserDefaults.standard.bool(forKey:
+  "hasCompletedProfile")
+      }
+  }
+
+
