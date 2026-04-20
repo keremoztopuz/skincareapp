c4c8f8953d03494d2b8b17f657c4278e2c6f4a5f
keremozt <keremoztopuzzz@gmail.com>
2026-04-20 22:01:18 +0300

    feat: add HomeViewModel with user profile fetch via LocalPersistenceManager

--- SkinCare/ViewModel/HomeViewModel.swift
+++ SkinCare/ViewModel/HomeViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreData
+internal import Combine
+
+class HomeViewModel: ObservableObject {
+    @Published var userName: String = ""
+    
+    init() {
+        fetchNames()
+    }
+    
+    func fetchNames() {
+        let profile = LocalPersistenceManager.shared.fetchUserProfile()
+        self.userName = profile?.name ?? ""
+        
+    }
+}
