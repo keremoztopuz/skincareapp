b36d6c4141acb41a5e833fbc699c67fba77a8a10
keremozt <keremoztopuzzz@gmail.com>
2026-04-27 23:26:04 +0300

    feat: update content viewmodel with isPremium state and improved app flow logic

--- SkinCare/ViewModel/ContentViewModel.swift
+++ SkinCare/ViewModel/ContentViewModel.swift
@@ -15,6 +15,7 @@ enum AppState {
     case profileSetup
     case loading
     case mainApp
+    case subscription
 }
 
 class ContentViewModel: ObservableObject {
@@ -28,10 +29,22 @@ class ContentViewModel: ObservableObject {
             UserDefaults.standard.set(hasCompletedProfile, forKey: "hasCompletedProfile")
         }
     }
-
+    @Published var hasCompletedSubscription: Bool {
+        didSet {
+            UserDefaults.standard.set(hasCompletedSubscription, forKey: "hasCompletedSubscription")
+        }
+    }
+    
+    
     @Published var showSplash = true
     @Published var showLoading = false
-
+    
+    @Published var isPremium: Bool {
+        didSet {
+            UserDefaults.standard.set(isPremium, forKey: "isPremium")
+        }
+    }
+    
     var currentState: AppState {
         if showSplash {
             return .splash
@@ -41,30 +54,47 @@ class ContentViewModel: ObservableObject {
             return .profileSetup
         } else if showLoading {
             return .loading
+        } else if !hasCompletedSubscription {
+            return .subscription
         } else {
             return .mainApp
         }
     }
-
+    
     init() {
         self.hasCompletedOnBoarding = UserDefaults.standard.bool(forKey: "hasCompletedOnBoarding")
         self.hasCompletedProfile = UserDefaults.standard.bool(forKey: "hasCompletedProfile")
-
-        // splash 4.5 saniye görünsün
+        self.hasCompletedSubscription = UserDefaults.standard.bool(forKey: "hasCompletedSubscription")
+        self.isPremium = UserDefaults.standard.bool(forKey: "isPremium")
+        
+        self.hasCompletedOnBoarding = false
+        self.hasCompletedProfile = false
+        self.hasCompletedSubscription = false
+        
+        // splash shows 4.5 sec
         DispatchQueue.main.asyncAfter(deadline: .now() + 4.5) { [weak self] in
             self?.showSplash = false
         }
     }
-
+    
     func completeOnBoarding() {
         hasCompletedOnBoarding = true
     }
-
+    
     func completeProfile() {
         hasCompletedProfile = true
         showLoading = true
         DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) { [weak self] in
             self?.showLoading = false
+        }
+    }
+    
+    func completePurchaseStep(isPremium: Bool) {
+        self.isPremium = isPremium
+        self.showLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) { [weak self] in
+            self?.showLoading = false
+            self?.hasCompletedSubscription = true
         }
     }
 }
