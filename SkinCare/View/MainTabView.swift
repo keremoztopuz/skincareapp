b833b048aee5b407e2dae8538698769530593f5b
keremozt <keremoztopuzzz@gmail.com>
2026-05-16 13:17:37 +0300

    Ana sekme yapısı ve navigasyon düzenlendi

--- SkinCare/View/MainTabView.swift
+++ SkinCare/View/MainTabView.swift
@@ -9,30 +9,36 @@ import Foundation
 import SwiftUI
 
 struct MainTabView: View {
+    @State private var selectedTab: Int = 0
     var body: some View {
         let tintColor = Color(red:0.47, green: 0.11, blue: 0.17)
         
-        TabView{
-            HomeView()
+        TabView(selection: $selectedTab){
+            HomeView(selectedTab: $selectedTab)
                 .tabItem {
                     Label("Home", systemImage: "house")
                 }
+                .tag(0)
             SearchView()
                 .tabItem {
                     Label("Search", systemImage: "magnifyingglass")
                 }
+                .tag(1)
             CameraView()
                 .tabItem {
                     Label("Camera", systemImage: "camera")
                 }
-            RecentsView()
+                .tag(2)
+            RecentsView(selectedTab: $selectedTab)
                 .tabItem {
                     Label("Recents", systemImage: "book")
                 }
+                .tag(3)
             MoreView()
                 .tabItem {
                     Label("Profile", systemImage: "person")
                 }
+                .tag(4)
         }
         .tint(Color(tintColor))
     }
