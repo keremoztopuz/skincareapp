e239ffbf3bb690f3435ffc170c71520b6c424b10
keremozt <keremoztopuzzz@gmail.com>
2026-05-16 13:18:18 +0300

    Genel uygulama testleri eklendi

---
+++ SkinCareTests/SkinCareTests.swift
@@ -0,0 +1,16 @@
+//
+//  SkinCareTests.swift
+//  SkinCareTests
+//
+//  Created by Kerem Öztopuz on 14.05.2026.
+//
+
+import Testing
+
+struct SkinCareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
